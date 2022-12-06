e94d62b4d7d2183040a2c391153295fbb9fd953f
stephen omarrah <stephenomarrah@gmail.com>
2022-12-05 22:33:06 -0600

    basic layout finished

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,28 @@
 import "../styles/globals.css";
 import Spline from '@splinetool/react-spline';
 import Header from "./component/Header";
-function MyApp({ Component, pageProps }) {
-  return (
-    <div>
-      <Header/>
-      <Component {...pageProps} />
-      <div className='bg' style={{ width: "100vw", height: "100vh", position:"absolute", zIndex:"-1", top:"0px"}}>
-          <Spline scene="https://prod.spline.design/g6MWV2TyeW6Emrqe/scene.splinecode" />
-      </div>
-    </div>
-  )
+
+function MyApp({ Component, pageProps, ...appProps }) {
+  const getContent = () => {
+    if ([`/about`].includes(appProps.router.pathname)){
+      return (
+        <>
+          <Header/>
+          <Component {...pageProps}/>
+        </>
+      )}else{
+      return (
+        <>
+          <Header/>
+          <Component {...pageProps} />
+          <div style={{ width: "100vw", height: "100vh", position:"absolute", zIndex:"0", top:"0px"}}>
+            <Spline scene="https://prod.spline.design/x-0dOPrwauNvnY-b/scene.splinecode" />
+          </div>
+        </>
+      )}
+  };
+  
+  return (<>{getContent()}</>)
  
 }
 
--- pages/about/index.js
+++ pages/about/index.js
@@ -1,7 +1,19 @@
-
+import Spline from '@splinetool/react-spline';
 
 export default function index() {
   return (
-    <div>About Page</div>
+    <>
+    <div className="glass" style={{width:"50%", height:"30%", position:"absolute", top:"30%", left:"50%", margin:"0"}}>
+      <div style={{position:"relative"}}>
+        About Page
+      </div>
+    </div>
+    <div style={{ width: "100vw", height: "100vh", position:"absolute", zIndex:"0", top:"0px"}}>
+    <Spline scene="https://prod.spline.design/qQl4PKhy6wbKVbAM/scene.splinecode" />
+  </div>
+  </>
   )
 }
+
+
+
